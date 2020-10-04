4a13b7b5370ca21415aea8361b6898651ea026c2
Королев Пётр <37357614+PitKoro@users.noreply.github.com>
2020-10-04 23:06:56 +0300

    Добавлено выделение активного пункта меню (#10)

--- core/views.py
+++ core/views.py
@@ -3,12 +3,15 @@ from .models import Account
 
 
 def test(request):
+    url_name = request.resolver_match.url_name
     account_list = Account.objects.all()
     print(account_list[0].name)
-    return render(request, 'core/test.html', {'account_list': account_list})
+    return render(request, 'core/test.html', {'account_list': account_list, 'url_name': url_name})
 
 def report(request):
-    return render(request, 'core/report.html')
+    url_name = request.resolver_match.url_name
+    return render(request, 'core/report.html', {'url_name': url_name})
 
 def history(request):
-    return render(request, 'core/history.html')
+    url_name = request.resolver_match.url_name
+    return render(request, 'core/history.html', {'url_name': url_name})
