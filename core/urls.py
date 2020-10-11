f9aa3ccf1161640ad7f23d1bb3856477ea20d336
sergiyvl <71441301+sergiyvl@users.noreply.github.com>
2020-10-11 17:18:56 +0300

    новый дизайн меню (#13)
    * новый дизайн меню
    
    * Починил переход на главную страницу (#14)
    
    * Исправил баг с переходом на главную страницу и удалил ненужное
    
    * Попытка добавить активный пункт меню
    
    * Поменял цвет активного пункта меню
    
    * Поменял aside на navbar, изменил цвета на темные (#15)
    
    * Поменял aside на navbar, изменил цвета на темные
    
    * Сделал горизонтальное меню
    
    * Удалил ширину aside, изменил размер колонок
    
    * Костыль в виде aside
    
    * Убрал nav, т.к. он динамически меняет ширину навигации в зависимости от ширины ссылки
    
    * Пернес стили логшотипа в css
    
    Co-authored-by: sergiyvl <bat313@yandex.ru>
    Co-authored-by: Королев Пётр <37357614+PitKoro@users.noreply.github.com>

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.test, name='test'),
+    path('', views.main, name='main'),
     path('report', views.report, name='report'),
     path('history', views.history, name='history')
 ]
