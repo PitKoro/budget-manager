738fd1de995aa6a9c41657efcdb1a70b35f34e14
MrFang <bat312@yandex.ru>
2020-10-16 10:50:27 +0300

    Add new models (#17)
    * Add new models
    
    * Make migrations

--- core/models.py
+++ core/models.py
@@ -4,17 +4,33 @@ from django.db import models
 class IncomeCategory(models.Model):
     name = models.CharField(max_length=100)
 
-    class Meta:
-        db_table = 'core_income_category'
-
 
 class ExpenseCategory(models.Model):
     name = models.CharField(max_length=100)
 
-    class Meta:
-        db_table = 'core_expense_category'
-
 
 class Account(models.Model):
     name = models.CharField(max_length=100)
+
+
+class Transaction(models.Model):
     amount = models.PositiveIntegerField()
+    commentaty = models.TextField(blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class IncomeTransaction(Transaction):
+    income_category = models.ForeignKey(IncomeCategory, on_delete=models.PROTECT)
+    account = models.ForeignKey(Account, on_delete=models.PROTECT)
+
+
+class ExpenseTransaction(Transaction):
+    account = models.ForeignKey(Account, on_delete=models.PROTECT)
+    expense_category = models.ForeignKey(ExpenseCategory, on_delete=models.PROTECT)
+
+
+class InnerTransaction(Transaction):
+    account_from = models.ForeignKey(Account, on_delete=models.PROTECT, related_name='inner_transaction_from_set')
+    accout_to = models.ForeignKey(Account, on_delete=models.PROTECT, related_name='inner_transaction_to_set')
