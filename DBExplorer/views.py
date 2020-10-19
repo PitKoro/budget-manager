150f1583bd08dfb31acfd48fbd42926752d06639
MrFang <bat312@yandex.ru>
2020-10-19 11:29:04 +0300

    Fix typo

--- DBExplorer/views.py
+++ DBExplorer/views.py
@@ -31,7 +31,7 @@ class ExpenseCategoryView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['model_name'] = 'Expence Category'
+        context['model_name'] = 'Expense Category'
 
         return context
 
