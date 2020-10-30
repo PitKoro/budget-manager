1a9455ae8a8f1c6627a4d88283416dd4993c42ba
Кочуров <kolya.kochurov.2017@mail.ru>
2020-10-30 22:37:35 +0300

    Вывел сумму расходов и зачислений по категориям зачислений и по оставшимся категориям расходов

--- core/views.py
+++ core/views.py
@@ -62,16 +62,53 @@ def report(request):
         expense_transaction_list.append(expense.amount)
     expense_summ = sum(expense_transaction_list)
 
-    expense_category_amount_list = []
+    expense_category_amount_list1 = []
     for el in ExpenseTransaction.objects.filter(expense_category_id=1):
-        expense_category_amount_list.append(el.amount)
-    expense_category_amount_summ=sum(expense_category_amount_list)
+        expense_category_amount_list1.append(el.amount)
+    expense_category_amount_summ1=sum(expense_category_amount_list1)
+
+    expense_category_amount_list2 = []
+    for el in ExpenseTransaction.objects.filter(expense_category_id=2):
+        expense_category_amount_list2.append(el.amount)
+    expense_category_amount_summ2=sum(expense_category_amount_list2)
+    
+    expense_category_amount_list3 = []
+    for el in ExpenseTransaction.objects.filter(expense_category_id=3):
+        expense_category_amount_list3.append(el.amount)
+    expense_category_amount_summ3=sum(expense_category_amount_list3)
+
+    expense_category_amount_list4 = []
+    for el in ExpenseTransaction.objects.filter(expense_category_id=4):
+        expense_category_amount_list4.append(el.amount)
+    expense_category_amount_summ4=sum(expense_category_amount_list4)
+
+    income_category_amount_list1 = []
+    for el in IncomeTransaction.objects.filter(income_category_id=1):
+        income_category_amount_list1.append(el.amount)
+    income_category_amount_summ1=sum(income_category_amount_list1)
+
+    income_category_amount_list2 = []
+    for el in IncomeTransaction.objects.filter(income_category_id=2):
+        income_category_amount_list2.append(el.amount)
+    income_category_amount_summ2=sum(income_category_amount_list2)
+
+    income_category_amount_list3 = []
+    for el in IncomeTransaction.objects.filter(income_category_id=3):
+        income_category_amount_list3.append(el.amount)
+    income_category_amount_summ3=sum(income_category_amount_list3)
+    
     
 
     return render(request, 'core/report.html', {'url_name': url_name, 
                                                 'income_summ': income_summ, 
                                                 'expense_summ': expense_summ,
-                                                'expense_category_amount_summ': expense_category_amount_summ})
+                                                'expense_category_amount_summ1': expense_category_amount_summ1,
+                                                'expense_category_amount_summ2': expense_category_amount_summ2,
+                                                'expense_category_amount_summ3': expense_category_amount_summ3,
+                                                'expense_category_amount_summ4': expense_category_amount_summ4,
+                                                'income_category_amount_summ1': income_category_amount_summ1,
+                                                'income_category_amount_summ2': income_category_amount_summ2,
+                                                'income_category_amount_summ3': income_category_amount_summ3})
 
 
 def history(request):
