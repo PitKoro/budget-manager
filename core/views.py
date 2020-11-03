0dc176e06942a1a0c0ce44dca064429208981894
Королев Пётр <37357614+PitKoro@users.noreply.github.com>
2020-11-03 14:45:43 +0300

    Удаление транзакций на странице истории (#34)
    * Удалил опцию "Период"
    
    * Реализовал возмножность удаления транзакций
    
    * добавил стилей для карточки транзакций и адаптировал страницу под ipad
    
    * Удалил ненужные классы
    
    * Добавил калассы для "Транзакций нет"
    
    * Сделал кнопку удаления всегда красной

--- core/views.py
+++ core/views.py
@@ -57,22 +57,40 @@ def report(request):
 
 def history(request):
     url_name = request.resolver_match.url_name
-    incomeT = IncomeTransaction.objects.all()
-    expenseT = ExpenseTransaction.objects.all()
-    innerT = InnerTransaction.objects.all()
-    transactions = sorted((chain(incomeT, expenseT, innerT)), key=attrgetter('date'), reverse=True)
 
     filter_value = "all"
 
     if request.method == 'POST':
-        filter_value=request.POST.get('history_filter')
-        if filter_value == "all":
-            return render(request, 'core/history.html', {'url_name': url_name, 'transactions':transactions, 'filter_value': filter_value})
-        if int(filter_value) in range(1,13):
-            incomeT = IncomeTransaction.objects.filter(date__month=int(filter_value))
-            expenseT = ExpenseTransaction.objects.filter(date__month=int(filter_value))
-            innerT = InnerTransaction.objects.filter(date__month=int(filter_value))
-            transactions = sorted((chain(incomeT, expenseT, innerT)), key=attrgetter('date'), reverse=True)
-            return render(request, 'core/history.html', {'url_name': url_name, 'transactions':transactions, 'filter_value': filter_value})
+        if request.POST.get('history_filter'):
+            filter_value=request.POST.get('history_filter')
+            if filter_value == "all":
+                incomeT = IncomeTransaction.objects.all()
+                expenseT = ExpenseTransaction.objects.all()
+                innerT = InnerTransaction.objects.all()
+                transactions = sorted((chain(incomeT, expenseT, innerT)), key=attrgetter('date'), reverse=True)
+                return render(request, 'core/history.html', {'url_name': url_name, 'transactions':transactions, 'filter_value': filter_value})
+            if int(filter_value) in range(1,13):
+                incomeT = IncomeTransaction.objects.filter(date__month=int(filter_value))
+                expenseT = ExpenseTransaction.objects.filter(date__month=int(filter_value))
+                innerT = InnerTransaction.objects.filter(date__month=int(filter_value))
+                transactions = sorted((chain(incomeT, expenseT, innerT)), key=attrgetter('date'), reverse=True)
+                return render(request, 'core/history.html', {'url_name': url_name, 'transactions': transactions, 'filter_value': filter_value})
+
+        if request.POST.get('IncomeTransactionId'):
+            IncomeTransactionId=int(request.POST.get('IncomeTransactionId'))
+            IncomeTransaction.objects.filter(id=IncomeTransactionId).delete()
+        
+        if request.POST.get('InnerTransactionId'):
+            InnerTransactionId=int(request.POST.get('InnerTransactionId'))
+            InnerTransaction.objects.filter(id=InnerTransactionId).delete()
+
+        if request.POST.get('ExpenseTransactionId'):
+            ExpenseTransactionId=int(request.POST.get('ExpenseTransactionId'))
+            ExpenseTransaction.objects.filter(id=ExpenseTransactionId).delete()
+
+    incomeT = IncomeTransaction.objects.all()
+    expenseT = ExpenseTransaction.objects.all()
+    innerT = InnerTransaction.objects.all()
+    transactions = sorted((chain(incomeT, expenseT, innerT)), key=attrgetter('date'), reverse=True)
 
     return render(request, 'core/history.html', {'url_name': url_name, 'transactions': transactions, 'filter_value': filter_value})
