9879e1d2e23e33c08090a811ae67f74d0d0decc5
MrFang <bat312@yandex.ru>
2020-11-10 18:27:54 +0300

    Latest transactions (#32)
    Add latest transaction block

--- core/utils.py
+++ core/utils.py
@@ -2,6 +2,10 @@ from django.db.models import Sum
 from django.db.models.functions import Coalesce
 from .models import Account, IncomeCategory, IncomeTransaction, InnerTransaction, ExpenseCategory, ExpenseTransaction
 
+from datetime import date, timedelta
+from functools import reduce
+from itertools import chain
+from operator import attrgetter
 
 def get_balance(account):
     outer_income = account.incometransaction_set.aggregate(
@@ -76,3 +80,74 @@ def post_expense_transaction(data):
             account_to=Account.objects.get(id=to_id)
         )
     transaction.save()
+
+
+def get_account_list():
+    account_list = []
+
+    for account in Account.objects.all():
+        account_list.append({
+            'name': account.name,
+            'amount': get_balance(account)/100
+        })
+    account_list.insert(0, {
+        'name': 'Всего',
+        'amount': reduce(
+            lambda acc, value: acc + value['amount'],
+            account_list,
+            0
+        )
+    })
+
+    return account_list
+
+
+def get_current_week_transactions():
+    date_to = date.today()
+    date_from = date_to - timedelta(days=date_to.weekday())
+
+    income_transactions = IncomeTransaction.objects.filter(
+        date__range=[date_from, date_to]
+    )
+    expense_transactions = ExpenseTransaction.objects.filter(
+        date__range=[date_from, date_to]
+    )
+    inner_transactions = InnerTransaction.objects.filter(
+        date__range=[date_from, date_to]
+    )
+
+    tran_list = sorted(
+        (chain(income_transactions, expense_transactions, inner_transactions)),
+        key=attrgetter('date'),
+        reverse=True
+    )
+
+    result = []
+    for transaction in tran_list:
+        type_name = ''
+        from_name = ''
+        to_name = ''
+
+        if isinstance(transaction, IncomeTransaction):
+            type_name = 'income'
+            from_name = transaction.income_category.name
+            to_name = transaction.account.name
+        elif isinstance(transaction, ExpenseTransaction):
+            type_name = 'expense'
+            from_name = transaction.account.name
+            to_name = transaction.expense_category.name
+        elif isinstance(transaction, InnerTransaction):
+            type_name = 'inner'
+            from_name = transaction.account_from.name
+            to_name = transaction.account_to.name
+
+        result.append({
+            'type': type_name,
+            'date': transaction.date,
+            'amount': transaction.amount / 100,
+            'from': from_name,
+            'to': to_name,
+            'commentary': transaction.commentary
+        })
+
+    return result
--- core/views.py
+++ core/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
+from .models import IncomeTransaction, ExpenseTransaction, InnerTransaction
+from .forms.IncomeForm import IncomeForm
+from .forms.ExpenseForm import ExpenseForm
+import core.utils as utils
+
 from itertools import chain
 from operator import attrgetter
-from .models import Account, IncomeTransaction, ExpenseTransaction, InnerTransaction
-from .forms.IncomeForm import IncomeForm
-from .forms.ExpenseForm import ExpenseForm 
-from .utils import get_balance, post_income_transaction, post_expense_transaction
-from functools import reduce
 
 
 def main(request):
@@ -19,31 +19,17 @@ def main(request):
             formEF = ExpenseForm(request.POST)
 
         if formIF.is_valid():
-            post_income_transaction(formIF.cleaned_data)
+            utils.post_income_transaction(formIF.cleaned_data)
             formIF = IncomeForm()
         if formEF.is_valid():
-            post_expense_transaction(formEF.cleaned_data)
+            utils.post_expense_transaction(formEF.cleaned_data)
             formEF = ExpenseForm()
 
     url_name = request.resolver_match.url_name
-    account_list = []
-
-    for account in Account.objects.all():
-        account_list.append({
-            'name': account.name,
-            'amount': get_balance(account)/100
-        })
-    account_list.insert(0, {
-        'name': 'Всего',
-        'amount': reduce(
-            lambda acc, value: acc + value['amount'],
-            account_list,
-            0
-        )
-    })
 
     return render(request, 'core/main.html', {
-        'account_list': account_list,
+        'account_list': utils.get_account_list(),
+        'latest_transactions': utils.get_current_week_transactions(),
         'url_name': url_name,
         'income_form': formIF,
         'expence_form': formEF
@@ -57,40 +43,22 @@ def report(request):
 
 def history(request):
     url_name = request.resolver_match.url_name
-
-    filter_value = "all"
-
-    if request.method == 'POST':
-        if request.POST.get('history_filter'):
-            filter_value=request.POST.get('history_filter')
-            if filter_value == "all":
-                incomeT = IncomeTransaction.objects.all()
-                expenseT = ExpenseTransaction.objects.all()
-                innerT = InnerTransaction.objects.all()
-                transactions = sorted((chain(incomeT, expenseT, innerT)), key=attrgetter('date'), reverse=True)
-                return render(request, 'core/history.html', {'url_name': url_name, 'transactions':transactions, 'filter_value': filter_value})
-            if int(filter_value) in range(1,13):
-                incomeT = IncomeTransaction.objects.filter(date__month=int(filter_value))
-                expenseT = ExpenseTransaction.objects.filter(date__month=int(filter_value))
-                innerT = InnerTransaction.objects.filter(date__month=int(filter_value))
-                transactions = sorted((chain(incomeT, expenseT, innerT)), key=attrgetter('date'), reverse=True)
-                return render(request, 'core/history.html', {'url_name': url_name, 'transactions': transactions, 'filter_value': filter_value})
-
-        if request.POST.get('IncomeTransactionId'):
-            IncomeTransactionId=int(request.POST.get('IncomeTransactionId'))
-            IncomeTransaction.objects.filter(id=IncomeTransactionId).delete()
-        
-        if request.POST.get('InnerTransactionId'):
-            InnerTransactionId=int(request.POST.get('InnerTransactionId'))
-            InnerTransaction.objects.filter(id=InnerTransactionId).delete()
-
-        if request.POST.get('ExpenseTransactionId'):
-            ExpenseTransactionId=int(request.POST.get('ExpenseTransactionId'))
-            ExpenseTransaction.objects.filter(id=ExpenseTransactionId).delete()
-
     incomeT = IncomeTransaction.objects.all()
     expenseT = ExpenseTransaction.objects.all()
     innerT = InnerTransaction.objects.all()
     transactions = sorted((chain(incomeT, expenseT, innerT)), key=attrgetter('date'), reverse=True)
+
+    filter_value = "all"
+
+    if request.method == 'POST':
+        filter_value=request.POST.get('history_filter')
+        if filter_value == "all":
+            return render(request, 'core/history.html', {'url_name': url_name, 'transactions':transactions, 'filter_value': filter_value})
+        if int(filter_value) in range(1,13):
+            incomeT = IncomeTransaction.objects.filter(date__month=int(filter_value))
+            expenseT = ExpenseTransaction.objects.filter(date__month=int(filter_value))
+            innerT = InnerTransaction.objects.filter(date__month=int(filter_value))
+            transactions = sorted((chain(incomeT, expenseT, innerT)), key=attrgetter('date'), reverse=True)
+            return render(request, 'core/history.html', {'url_name': url_name, 'transactions':transactions, 'filter_value': filter_value})
 
     return render(request, 'core/history.html', {'url_name': url_name, 'transactions': transactions, 'filter_value': filter_value})
